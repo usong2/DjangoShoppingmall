c631c664a1930000fc58a0f0915058bb937f76cd
usong2 <dbthd6@naver.com>
2020-03-25 00:18:34 +0900

    login

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.hashers import check_password, make_password
 from .models import User
 
 class RegisterForm(forms.Form):
@@ -34,6 +35,37 @@ class RegisterForm(forms.Form):
             else:
                 user = User(
                     email=email,
-                    password=password
+                    password=make_password(password)
                 )
                 user.save()
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64, label='이메일'
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label='비밀번호'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                self.add_error('email', '아이디가 없습니다.')
+                return 
+
+            if not check_password(password, user.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
+            else:
+                self.email = user.email
