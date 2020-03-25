be4ee55e9b195f37bf07b09bd22b892aa0ff8b9e
usong2 <dbthd6@naver.com>
2020-03-25 22:10:32 +0900

    product register1

--- ys_django/urls.py
+++ ys_django/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from user.views import index, RegisterView, LoginView
-from product.views import ProductList
+from product.views import ProductList, ProductCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('product/', ProductList.as_view()),
+    path('product/create/', ProductCreate.as_view()),
 ]
