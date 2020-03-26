0c0e3e9f65565aa88459f4fa198f389fc33e8150
usong2 <dbthd6@naver.com>
2020-03-26 22:49:04 +0900

    order save

--- order/forms.py
+++ order/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Order
+from product.models import Product
+from user.models import User
 
 class RegisterForm(forms.Form):
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+    
     quantity = forms.IntegerField(
         error_messages={
             'required': '수량을 입력해주세요.'
@@ -17,4 +23,17 @@ class RegisterForm(forms.Form):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
-        
+        user = self.request.session.get('user')
+
+        if quantity and product and user:
+            order = Order(
+                quantity=quantity,
+                product=Product.objects.get(pk=product),
+                user=User.objects.get(email=user)
+            )
+            order.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다')
+            self.add_error('product', '값이 없습니다')
+    
