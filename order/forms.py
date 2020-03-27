632b74dc9347396057f48faa85ba73ffe4f27ec6
usong2 <dbthd6@naver.com>
2020-03-28 01:48:49 +0900

    refactoring

--- order/forms.py
+++ order/forms.py
@@ -24,21 +24,8 @@ class RegisterForm(forms.Form):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
-        user = self.request.session.get('user')
 
-        if quantity and product and user:
-            with transaction.atomic():
-                prod = Product.objects.get(pk=product)
-                order = Order(
-                    quantity=quantity,
-                    product=prod,
-                    user=User.objects.get(email=user)
-                )
-                order.save()
-                prod.stock -= quantity
-                prod.save()
-        else:
-            self.product = product
+        if not (quantity and product):
             self.add_error('quantity', '값이 없습니다')
             self.add_error('product', '값이 없습니다')
     
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.contrib.auth.hashers import make_password
 from .forms import RegisterForm, LoginForm
+from .models import User
 
 # Create your views here.
 
@@ -13,13 +15,24 @@ class RegisterView(FormView):
     form_class = RegisterForm 
     success_url = '/'
 
+    def form_valid(self, form):
+        user = User(
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password')),
+            level='user'
+        )
+        user.save()
+
+        return super().form_valid(form)
+
+
 class LoginView(FormView):
     template_name = 'login.html'
     form_class = LoginForm
     success_url = '/'
 
     def form_valid(self, form):
-        self.request.session['user'] = form.email
+        self.request.session['user'] = form.data.get('email')
 
         return super().form_valid(form)
 
